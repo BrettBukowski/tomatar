0dc2afd62b213d62595f40e19674b7986ab74538
Brett Bukowski <brett.bukowski@gmail.com>
2013-06-30 15:52:27 -0600

    ✔ User can sign out

--- public/js/services/User.js
+++ public/js/services/User.js
@@ -2,12 +2,16 @@ define(['app'], function (app) {
   "use strict";
 
   return app.service('userService', ['$http', '$cookies', function (http, cookies) {
+    var signedIn = cookies.signin == 'true';
+
     this.signedIn = function () {
-      return cookies.signin == 'true';
+      return signedIn;
     };
 
     this.signOut = function () {
-      http.post('/signout')
+      http.post('/signout').success(function () {
+        signedIn = false;
+      });
     };
   }]);
 });
