df5f12df504fdc2e408cb7e4b93e9a98339e7ae3
Brett Bukowski <brett.bukowski@gmail.com>
2013-08-26 20:07:51 -0600

    Jump between days

--- public/js/controllers/History.js
+++ public/js/controllers/History.js
@@ -2,9 +2,68 @@ define(['jquery', 'app'], function ($, app) {
   "use strict";
 
   return app.controller('HistoryController',
-    ['$scope', 'historyService', 'settingsService', 'dialogService',
-    function (scope, historyService, settingsService, Dialog) {
+    ['$scope', 'historyService', 'settingsService', 'dialogService', '$animate',
+    function (scope, historyService, settingsService, Dialog, animateService) {
       var detailsDialog;
+
+      function setAdjacentDay (index) {
+        var nextDay = scope.dayDetail.month.days[index];
+
+        if (nextDay) {
+          setDayDetail(nextDay, index, scope.dayDetail.month);
+          return true;
+        }
+
+        return false;
+      }
+
+      function setFirstDayFromNextMonth () {
+        var nextMonth;
+        scope.entries.some(function (month, i, months) {
+          if (month.$$hashKey == scope.dayDetail.month.$$hashKey) {
+            return nextMonth = months[i - 1];
+          }
+        });
+
+        if (nextMonth) {
+          setDayDetail(nextMonth.days[0], 0, nextMonth);
+          return true;
+        }
+
+        return false;
+      }
+
+      function setLastDayFromPreviousMonth () {
+        var prevMonth;
+        scope.entries.some(function (month) {
+          if (month.$$hashKey > scope.dayDetail.month.$$hashKey) {
+            return prevMonth = month;
+          }
+        });
+
+        if (prevMonth) {
+          var lastDay = prevMonth.days.length - 1;
+          setDayDetail(prevMonth.days[lastDay], lastDay, prevMonth);
+          return true;
+        }
+
+        return false;
+      }
+
+      function setDayDetail (day, index, month) {
+        var container = $('.completed-pomodori'),
+            className = 'loading';
+
+        animateService.addClass(container, className);
+        scope.dayDetail = {
+          // All finished sessions will share the same date.
+          date:     day.finished[0].date,
+          details:  day.finished,
+          index:    index,
+          month:    month
+        };
+        animateService.removeClass(container, className);
+      }
 
       settingsService.get().then(function (settings) {
         scope.hourFormat = settings.ui.hours;
@@ -33,16 +92,20 @@ define(['jquery', 'app'], function ($, app) {
       };
 
       scope.showDayDetail = function () {
-        scope.dayDetail = {
-          // All finished sessions will share the same date.
-          date:    this.day.finished[0].date,
-          details: this.day.finished
-        };
+        setDayDetail(this.day, this.$index, this.$parent.monthInterval);
       };
 
       scope.hideDayDetail = function (e) {
         e.preventDefault();
         scope.initDayDetail();
+      };
+
+      scope.nextDay = function () {
+        setAdjacentDay(scope.dayDetail.index + 1) || setFirstDayFromNextMonth();
+      };
+
+      scope.previousDay = function () {
+        setAdjacentDay(scope.dayDetail.index - 1) || setLastDayFromPreviousMonth();
       };
 
       scope.$on('$includeContentLoaded', function () {
